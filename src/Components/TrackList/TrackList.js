f49eae0f369993a9f3a2abe282242c3582aef8d7
Rick Mays <8709540+rickmays@users.noreply.github.com>
2022-09-01 10:52:05 -0500

    pass down search result and render result list

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -2,11 +2,22 @@ import React from 'react';
 
 import './TrackList.css';
 
+import Track from '../Track/Track';
+
 class TrackList extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div className="TrackList">
-        {/* add map method that renders a set of Track components */}
+        {
+          this.props.tracks.map(track => {
+            return <Track track={track}
+                          key={track.id} />
+          })
+        }
       </div>
     )
   }
