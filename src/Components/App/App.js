63e87082e5f6212c27c3637abb48f73ac44f01db
Rick Mays <8709540+rickmays@users.noreply.github.com>
2022-09-02 11:23:22 -0500

    create search functions in app and searchbar

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -55,6 +55,7 @@ class App extends React.Component {
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track) {
@@ -65,7 +66,6 @@ class App extends React.Component {
       tracks.push(track);
       this.setState({ playlistTracks: tracks });
     }
-
   }
 
   removeTrack(track) {
@@ -82,12 +82,16 @@ class App extends React.Component {
     const trackURIs = this.state.playlistTracks.map(track => track.uri);
   }
 
+  search(term) {
+    console.log(term);
+  }
+
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} 
               onAdd={this.addTrack}/>
